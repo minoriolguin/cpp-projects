1cde8a530f5f5bbfa02a1b04085547b0b607d852
Minori Olguin <minoriiolguin@gmail.com>
2024-11-25 22:25:33 -0700

    Completed classes inventory and item
    - may need to make minor changes as other classes are implemented

--- A4/Item.h
+++ A4/Item.h
@@ -2,18 +2,34 @@
 #define ITEM_H
 
 #include <iostream> // Stream declarations
+#include <vector>
 using namespace std;
 
 class Item
 {
 private:
-
+    string name;
+    vector<string> item_actions;
 
 public:
-    Item () {
-
+    Item() : name("") {}
+    
+    Item(string name) 
+    { 
+        this->name = name;  // remove this from constructor to add items that cannot be added to inventory to the game
     }
 
+    // Currently all items can be added to inventory, but for scalability, if there's one that cannot
+    void addToInventoryItem() 
+    { 
+        item_actions.push_back("take"); 
+        item_actions.push_back("drop"); 
+    }
+
+    void addItemAction(string action) { item_actions.push_back(action); }
+    string getName() { return name; }
+    vector<string> getItemActions() { return item_actions; } // this function may be unnecessary review code and double check ** REMOVE ME BEFORE SUBMITTING UNLESS NECESSARY
+    bool isItemAction(string action) { return find(item_actions.begin(), item_actions.end(), action)!= item_actions.end(); }
 };
 
 #endif
